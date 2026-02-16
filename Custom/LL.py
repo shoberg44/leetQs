c01baf2b32da4b59403733b52434861a62e5124f
Steven Hoberg <steven.hoberg1@gmail.com>
2026-02-16 15:54:25 -0500

    LL

--- Custom/LL.py
+++ Custom/LL.py
@@ -1,11 +1,54 @@
 class LList:
+    def __init__(self):
+        self._head = None
+        self._tail = None
+    def push(self, data):
+        data_node = self.Node(data)
+        if not self._tail:
+            self._tail = data_node
+        else:
+            # update tail
+            prev = self._tail
+            self._tail = data_node
+
+            # link prev node
+            prev.next = data_node
+            data_node.prev = prev
+        if not self._head:
+            self._head = data_node
+
+
+    def pop(self, front=False):
+        # if empty
+        if not self._head:
+            return None
+
+        # reassign tail to tails prev
+        target = self._tail
+        self._tail = target.prev
+        # remove links
+        target.prev.next = None
+        return target.data
+    def __str__(self):
+        current_node = self._head
+        ret = []
+        while current_node.next:
+            ret.append(str(current_node.data))
+            current_node = current_node.next
+        ret.append(str(current_node.data))
+        return ' -> '.join(ret)
+
+
+
+
+    '''
+    A class to track objects in the Linked List
+    '''
+
     class Node:
         next = None
         prev = None
         data = None
-    def __init__(self):
-        pass
-    def push(self, data):
-        print(data)
-    def pop(self, front=False):
-        pass
+
+        def __init__(self, data):
+            self.data = data
--- Custom/main.py
+++ Custom/main.py
@@ -2,6 +2,12 @@ from LL import LList
 from Med.LRU import LRUCache
 hello = LList()
 hello.push(1)
+hello.push(2)
+print(hello)
+hello.push(4)
+print(hello.pop())
+hello.push(-1) # 1,2,4,-1
+print(hello)
 
 lRUCache = LRUCache(2)
 print(lRUCache.put(1, 1)) # cache is {1=1}
@@ -13,3 +19,5 @@ print(lRUCache.put(4, 4)) # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
 print(lRUCache.get(1))   # return -1 (not found)
 print(lRUCache.get(3))    # return 3
 print(lRUCache.get(4))    # return 4
+
+
