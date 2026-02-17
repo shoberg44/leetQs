e226c93b004a8996c3fa680d75343e1a2098af13
Steven Hoberg <steven.hoberg1@gmail.com>
2026-02-17 11:39:07 -0500

    save

---
+++ Custom/Tree.py
@@ -0,0 +1,27 @@
+# self balancing BSTs
+class RBTree:
+    pass
+class AVLTree:
+    '''
+    Inserts a Node onto a tree (by default) or another subtree defined by a head Node
+    '''
+    def insert(self, Node = None):
+        pass
+    '''
+    Right Rotates
+    '''
+    def _Rrot(self, node):
+        pass
+    '''
+    Left Rotates
+    '''
+    def _Lrot(self, node):
+        pass
+    '''
+    Balances a subtree
+    '''
+    def _balance(self, node):
+        pass
+class Node:
+    def __del__(self, key):
+        pass
--- Custom/main.py
+++ Custom/main.py
@@ -1,23 +1,26 @@
 from LL import LList
 from Med.LRU import LRUCache
-hello = LList()
-hello.push(1)
-hello.push(2)
-print(hello)
-hello.push(4)
-print(hello.pop())
-hello.push(-1) # 1,2,4,-1
-print(hello)
 
-lRUCache = LRUCache(2)
-print(lRUCache.put(1, 1)) # cache is {1=1}
-print(lRUCache.put(2, 2)) # cache is {1=1, 2=2}
-print(lRUCache.get(1))    # return 1
-print(lRUCache.put(3, 3)) # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-print(lRUCache.get(2))    # returns -1 (not found)
-print(lRUCache.put(4, 4)) # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-print(lRUCache.get(1))   # return -1 (not found)
-print(lRUCache.get(3))    # return 3
-print(lRUCache.get(4))    # return 4
+def testLinkedList():
+    hello = LList()
+    hello.push(1)
+    hello.push(2)
+    print(hello)
+    hello.push(4)
+    print(hello.pop())
+    hello.push(-1) # 1,2,4,-1
+    print(hello)
+
+def testLRU():
+    lRUCache = LRUCache(2)
+    print(lRUCache.put(1, 1)) # cache is {1=1}
+    print(lRUCache.put(2, 2)) # cache is {1=1, 2=2}
+    print(lRUCache.get(1))    # return 1
+    print(lRUCache.put(3, 3)) # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    print(lRUCache.get(2))    # returns -1 (not found)
+    print(lRUCache.put(4, 4)) # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    print(lRUCache.get(1))   # return -1 (not found)
+    print(lRUCache.get(3))    # return 3
+    print(lRUCache.get(4))    # return 4
 
 
